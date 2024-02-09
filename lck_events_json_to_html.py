6bf1a49940bd18ed7adb73a5b0d92a2af2c8b9c5
장주혜[기술문서] <joohye.jang@nts-corp.com>
2024-02-09 13:03:39 +0900

    .
    .
    
    Delete README.md
    
    Delete example.py
    
    Delete lolesports_api.py
    
    Delete test_lolesports_api.py
    
    Delete results.txt
    
    .

---
+++ lck_events_json_to_html.py
@@ -0,0 +1,130 @@
+from os import chdir
+from os.path import dirname
+
+chdir(dirname(__file__))
+
+import json
+from datetime import datetime
+
+def lck_events_json_to_html():
+    with open('lck_events.json', 'r') as f:
+        events = json.load(f)
+
+    against = {}
+    schedule = {}
+    points = {}
+    diffs = {}
+    teams = ['GEN', 'T1', 'KT', 'HLE', 'DK', 'DRX', 'FOX', 'BRO', 'NS', 'KDF']
+
+    for event in events:
+        if datetime.strptime(event['startTime'], "%Y-%m-%dT%H:%M:%SZ") < datetime(2024, 1, 1):
+            continue
+        if not event['blockName'].startswith('Week'):
+            continue
+
+        home = event['match']['teams'][0]['code']
+        away = event['match']['teams'][1]['code']
+
+        if event['state'] == 'completed':
+            point = 1 if event['match']['teams'][0]['result']['outcome'] == 'win' else -1
+            diff = event['match']['teams'][0]['result']['gameWins'] - event['match']['teams'][1]['result']['gameWins']
+
+            points[home] = points.setdefault(home, 0) + point
+            points[away] = points.setdefault(away, 0) - point
+
+            diffs[home] = diffs.setdefault(home, 0) + diff
+            diffs[away] = diffs.setdefault(away, 0) - diff
+
+            against.setdefault(home, {}).setdefault(away, []).append(point)
+            against.setdefault(away, {}).setdefault(home, []).append(-point)
+
+            schedule.setdefault(home, []).append({'vs':away, 'diff':diff})
+            schedule.setdefault(away, []).append({'vs':home, 'diff':-diff})
+
+        else:
+            schedule.setdefault(home, []).append({'vs':away, 'diff':None})
+            schedule.setdefault(away, []).append({'vs':home, 'diff':None})
+
+    teams.sort(key=lambda x: (points[x], diffs[x]), reverse=True)
+
+    for team in teams:
+        print(f'{team}\t{points[team]}\t{diffs[team]}')
+
+    str = '''<!DOCTYPE html>
+    <html>
+    <head>
+    <style>
+    table {margin: 20px;}
+    th {text-align: center; width: 42px;}
+    td {text-align: center; width: 36px;}
+    tr {border-bottom: 1px solid #888;}
+    table, th, td {border-collapse: collapse;}
+    td.ww {background-color: hsl(200, 100%, 80%);}
+    td.w {background-color: hsl(200, 100%, 90%);}
+    td.wl, td.lw {background-color: hsl(50, 100%, 80%);}
+    td.l {background-color: hsl(20, 100%, 90%);}
+    td.ll {background-color: hsl(20, 100%, 80%);}
+    td.na {background-color: #eee;}
+
+    table.schedule {border: 1px solid #aaa;}
+    col.border-right {border-right: 1px solid #aaa;}
+    table.schedule tr {border-bottom: 1px solid #aaa;}
+    </style>
+    </head>
+    <body>
+    <table><tr><td> </td>'''
+
+    for team in teams:
+        str += f'<td>{team}</td>'
+
+    str += '<th>Pts</th>'
+
+    for team in teams:
+        str += f'</tr>\n<tr><th>{team}</th>'
+        for vs in teams:
+            if team == vs:
+                str += '<td class="na">-</td>'
+                continue
+
+            text = ''
+            style = ''
+            for point in against[team].get(vs) or []:
+                if point == 1:
+                    text = text + '-' + 'W' if text else 'W'
+                    style += 'w'
+                else:
+                    text = text + '-' + 'L' if text else 'L'
+                    style += 'l'
+
+            str += f'<td class="{style}">{text}</td>'
+
+        str += f'<td>{points[team]}</td>'
+
+    str += '</tr></table>'
+    str += f'<table class="schedule"><colgroup><col class="border-right" /><col span="{len(teams)-2}"/><col class="border-right" /><col span="{len(teams)-2}"/><col class="border-right" /></colgroup>'
+    str += f'<tr><td> </td><th colspan="{len(teams)-1}">Round 1</th><th colspan="{len(teams)-1}">Round 2</th><th>Pts</th></tr>'
+
+    for team in teams:
+        str += f'<tr><th>{team}</th>'
+        for match in schedule[team]:
+            match match['diff']:
+                case 2:
+                    str += f'<td class="ww">{match["vs"]}</td>'
+                case 1:
+                    str += f'<td class="w">{match["vs"]}</td>'
+                case -1:
+                    str += f'<td class="l">{match["vs"]}</td>'
+                case -2:
+                    str += f'<td class="ll">{match["vs"]}</td>'
+                case _:
+                    str += f'<td>{match["vs"]}</td>'
+
+        str += f'<td>{points[team]}</td></tr>'
+
+    str += "</tr></table></body></html>"
+
+    with open("results.html", "w") as html_file:
+        html_file.write(str)
+
+if __name__ == "__main__":
+    lck_events_json_to_html()
---
+++ lck_events_to_json.py
@@ -0,0 +1,26 @@
+from os import chdir
+from os.path import dirname
+
+chdir(dirname(__file__))
+
+from lolesports_api import Lolesports_API
+import json
+
+def lck_events_to_json():
+
+    api = Lolesports_API()
+    data = api.get_schedule(league_id=98767991310872058)
+
+    pageToken = data['schedule']['pages']['newer']
+    events = data['schedule']['events']
+
+    while pageToken:
+        data = api.get_schedule(league_id=98767991310872058, pageToken=pageToken)
+        pageToken = data['schedule']['pages']['newer']
+        events += data['schedule']['events']
+
+    with open("lck_events.json", "w") as f:
+        json.dump(events, f, ensure_ascii=False, indent=4)
+
+if __name__ == "__main__":
+    lck_events_to_json()
